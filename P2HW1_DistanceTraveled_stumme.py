817172fa4b08a143635092eab2eb89e3a6ed472e
DrRachel <36680125+DrRachel@users.noreply.github.com>
2018-02-20 16:49:45 -0500

    Add files via upload
    First submission

---
+++ P2HW1_DistanceTraveled_stumme.py
@@ -0,0 +1,13 @@
+# P1T1
+# Hello World
+# Rachel Stumme
+# 2/18/2018
+
+distance6 = 6
+distance10 = 10
+distance15 = 15
+speed = 70
+
+print("the distance traveled after 6 hours is", distance6 * speed)
+print("the distance traveled after 10 hours is", distance10 * speed)
+print("the distance traveled after 15 hours is", distance15 * speed)
